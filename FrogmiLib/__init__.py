babea220a99c65438e66e58b8bb6121f86f6d2bc
John Elvis Arteaga Robles <john.arteagarobles@gmail.com>
2025-01-06 18:36:16 -0500

    18:36

---
+++ FrogmiLib/__init__.py
@@ -0,0 +1,20 @@
+#from .funciones import getToken
+
+def OSVersion():
+    det = """Version 1.0 =============
+    Libreria para agilizar el uso de las api de frogmi 5.1.2025
+    USO:
+    Consultar con John Arteaga
+    
+    Nota importante _____
+    Para modificar las columnas que se retornan, se debe editar el archivo funciones.py
+    agregar las columnas en la funcion correspondiente y volver a compilar el archivo WHL:
+
+    --> python .\setup.py bdist_wheel
+
+    Publicar en GITHUB para obtener la version linux del archivo WHL y cagar en el Lakehouse
+    """
+
+    
+    return det
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FrogmiLib",                # Nombre del paquete
+    version="0.1",                      # Versión inicial
+    description="Descripción de FrogmiLib",  # Breve descripción
+    author="John Arteaga",                 # Autor
+    packages=find_packages(),           # Encuentra automáticamente todos los módulos
+    install_requires=[],                # Dependencias, si las hay
+    python_requires='>=3.6',            # Versión mínima de Python
+)
