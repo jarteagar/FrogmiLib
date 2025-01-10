8b7b48e9aca7944b0f398c3f2ccb0203f5eec00c
John Elvis Arteaga Robles <john.arteagarobles@gmail.com>
2025-01-10 11:57:49 -0500

    0.8-11:57

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FrogmiLib",                # Nombre del paquete
-    version="0.7",                      # Versión inicial
+    version="0.8",                      # Versión inicial
     description="Descripción de FrogmiLib",  # Breve descripción
     author="John Arteaga",                 # Autor
     packages=find_packages(),           # Encuentra automáticamente todos los módulos
